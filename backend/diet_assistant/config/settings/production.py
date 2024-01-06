c7ce6dba7130390a6f6328244c6dae0e6dcf9fda
klitman <kornel.litman@gmail.com>
2024-01-06 20:59:56 +0100

    WIP: kubernetes & gh actions

--- backend/diet_assistant/config/settings/production.py
+++ backend/diet_assistant/config/settings/production.py
@@ -171,27 +171,6 @@ ADMIN_URL = env("DJANGO_ADMIN_URL", default="admin")
 # ------------------------------------------------------------------------------
 # https://django-storages.readthedocs.io/en/latest/#installation
 INSTALLED_APPS += ["storages"]  # noqa F405
-# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-AWS_ACCESS_KEY_ID = None  # Set to None to use IAM role; env('DJANGO_AWS_ACCESS_KEY_ID')
-# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-AWS_SECRET_ACCESS_KEY = (
-    None  # Set to None to use IAM role; env('DJANGO_AWS_SECRET_ACCESS_KEY')
-)
-# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-AWS_S3_REGION_NAME = env("DJANGO_AWS_S3_REGION_NAME")
-# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-# MEDIA
-AWS_STORAGE_MEDIA_BUCKET_NAME = env("DJANGO_AWS_STORAGE_MEDIA_BUCKET_NAME")
-AWS_MEDIA_QUERYSTRING_AUTH = env.bool("DJANGO_AWS_MEDIA_QUERYSTRING_AUTH", default=True)
-AWS_DEFAULT_ACL_MEDIA = env("DJANGO_AWS_DEFAULT_ACL_MEDIA", default="private")
-# DO NOT change these unless you know what you're doing.
-_AWS_EXPIRY = 60 * 60 * 24 * 7
-# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html#settings
-AWS_S3_OBJECT_PARAMETERS = {
-    "CacheControl": f"max-age={_AWS_EXPIRY}, s-maxage={_AWS_EXPIRY}, must-revalidate"
-}
-# https://github.com/jschneier/django-storages/issues/936
-AWS_S3_MAX_MEMORY_SIZE = env.int("DJANGO_AWS_S3_MEMORY_SIZE", default=1024 * 1024 * 4)
 
 # STATIC
 # ------------------------------------------------------------------------------
@@ -199,7 +178,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # MEDIA
 # ------------------------------------------------------------------------------
-DEFAULT_FILE_STORAGE = "config.storages.MediaRootS3Boto3Storage"
+# DEFAULT_FILE_STORAGE = "config.storages.MediaRootS3Boto3Storage"
 
 # DJANGO PROMETHEUS
 # ------------------------------------------------------------------------------
