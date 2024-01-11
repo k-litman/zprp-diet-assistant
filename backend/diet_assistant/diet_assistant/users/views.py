89ec00008c8d9c40f1c74cd4a6bb9d1a6f35216f
Wojciech Kołodziejak <wojtekk61@gmail.com>
2024-01-11 20:29:43 +0100

    Add basic locust test

--- backend/diet_assistant/diet_assistant/users/views.py
+++ backend/diet_assistant/diet_assistant/users/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from diet_assistant.users.serializers import UserSerializer
 
@@ -10,3 +10,12 @@ User = get_user_model()
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            # Allow any access for POST requests
+            return [permissions.AllowAny()]
+        else:
+            # For other methods, use the default permission set
+            # Example: permissions.IsAuthenticated()
+            return [permissions.IsAuthenticated()]
--- backend/load_tests/locustfile.py
+++ backend/load_tests/locustfile.py
@@ -1,60 +1,70 @@
-import os
+import random
+import time
 
-from load_tests.client import LoadTestClient
-from load_tests.mixins import TestCaseBase, UserApiMixin
-from load_tests.utils import ErrorResponse, load_users_credentials
-from locust import HttpLocust, TaskSet, task
-from locust.exception import LocustError
+import locust.exception
+from locust import HttpUser, task, events, run_single_user, between
+from faker import Faker
+from time import sleep
+fake = Faker()
+
+DEBUG = False
 
 
-class LoadTestTaskSet(TestCaseBase, UserApiMixin, TaskSet):
+def log(*args, **kwargs):
+    if DEBUG:
+        return print(*args, **kwargs)
+
+
+class CreateDietPlan(HttpUser):
+    wait_time = between(1, 5)
+    host = "http://0.0.0.0:8000"
+    weight = 10
+
+    def create_user(self):
+        self.user_id = random.randint(0, 1000000)
+        self.client.post(
+            "/users/",
+            json={
+                "username": f"test_user_{self.user_id}",
+                "password": "test_password",
+                "name": "test_name",
+            },
+        )
+
+    def login(self):
+        pass
+
+    def on_start(self):
+        self.create_user()
+        self.client.headers["Authorization"] = "Bearer e3ebcaa73ed91cb41f43e1d0007466c80d4a0caa"
+        self.login()
+        # log(f"ORDERING Created user with id {self.user_id} and delivery address")
+
     @task
-    def main_task(self):
-        # login user
-        if self.parent.AUTH and not self.login():
-            return
+    def e2e(self):
+        initial_count = self.client.get("/diets/diet-plans/").json()["count"]
+        self.client.post(
+            "/diets/diet-plans/",
+            json={
+                "name": "MY_diet_plan",
+                "days": 7,
+                "meals_per_day": 3,
+                "cuisine_type": "italian",
+                "veganity": {
+                    "vegan": True
+                },
+                "restricted_ingredients": [
+                    "Ingredient 1"
+                ],
+                "calories": 100
+            }
+        )
+        last_count = initial_count
+        while last_count == initial_count:
+            last_count = self.client.get("/diets/diet-plans/").json()["count"]
 
-        try:
-            # executes main flow
-            self.test_flow()
-        except ErrorResponse:
-            # handling error response
-
-            # if any error happens, should logout user and wait for respawn
-            if self.parent.AUTH:
-                self.logout()
-            return
-        except Exception:
-            # handling unexpected error
-
-            # if any error happens, should logout user and wait for respawn
-            if self.parent.AUTH:
-                self.logout()
-            raise
-
-        # after whole test case flow, user should logout
-        if self.parent.AUTH:
-            self.logout()
+        self.client.get("/diets/meals/")
 
 
-class LoadTests(HttpLocust):
-    def __init__(self):
-        super().__init__()
-        if self.host is None:
-            raise LocustError(
-                "You must specify the base host. "
-                "Either in the host attribute in the Locust class, "
-                "or on the command line using the --host option."
-            )
-
-        self.client = LoadTestClient(
-            base_url=self.host
-        )  # override normal client class with our extended one
-
-    (
-        AUTH,
-        CREDENTIALS,
-    ) = load_users_credentials()  # load user credentials from user dump file
-    task_set = LoadTestTaskSet  # our class with test case
-    min_wait = int(os.environ.get("LOAD_TEST_MIN_WAIT", default=5000))
-    max_wait = int(os.environ.get("LOAD_TEST_MAX_WAIT", default=9000))
+if __name__ == "__main__":
+    run_single_user(CreateDietPlan)
