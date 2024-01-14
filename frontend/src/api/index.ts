e7d4c3f8a5631de6af90e414a15f445b67c754b8
Mateusz <m.maj6743@gmail.com>
2024-01-14 21:13:32 +0100

    fix: fixed api client

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,12 +1,3 @@
-import axios from 'axios';
-
 export const API_KEYS = {
     DIET_PLANS: 'DIET_PLANS',
 };
-
-export const client = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    },
-});
--- frontend/src/pages/NewDietModal/NewDietModal.tsx
+++ frontend/src/pages/NewDietModal/NewDietModal.tsx
@@ -2,7 +2,6 @@ import React, { useState, useRef } from 'react';
 import { useQueryClient } from 'react-query';
 import { useAppSelector } from '@/hooks/useAppSelector';
 import { capitalize } from 'lodash';
-import { client } from '@/api';
 import axios from 'axios';
 
 interface ErrorResponse {
@@ -45,7 +44,7 @@ const NewDietModal = () => {
         try {
             if (!modalRef || !modalRef.current) return;
 
-            await client.post('/diets/diet-plans/', newDiet, {
+            await axios.post('/api/diets/diet-plans/', newDiet, {
                 headers: { Authorization: `Bearer ${authToken}` },
             });
             queryClient.invalidateQueries('diets');
